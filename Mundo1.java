78ed3dce4be80bcf7936c8049fe4e807eefa4c98
Jhonatan Morais <jhonatanvinicius@gmail.com>
2016-04-14 21:58:08 -0300

    Primeiro Commit

---
+++ Mundo1.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mundo1 extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public Mundo1()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(700, 390, 1); 
+        GreenfootImage cenario = new GreenfootImage("mundo1.png");
+        setBackground(cenario);
+        
+        
+    }
+}
